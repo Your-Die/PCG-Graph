091a813e02046a544396d3e7bd7538854c7c8bb1
Jordy <jordy_van_opstal@live.nl>
2020-12-11 14:18:34 +0100

    Replace FloatRangeGeneratorNode with FloatDistributionNode

---
+++ Primitives/FloatDistributionNode.cs
@@ -0,0 +1,19 @@
+﻿using Chinchillada.Distributions;
+using Sirenix.Serialization;
+
+namespace Chinchillada.GeneratorGraph
+{
+    public class FloatDistributionNode : GeneratorNodeWithPreview<float>
+    {
+        [OdinSerialize] private IDistribution<float> distribution;
+        
+        protected override float GenerateInternal()
+        {
+            return this.distribution.Sample();
+        }
+
+        protected override void UpdateInputs()
+        {
+        }
+    }
+}
