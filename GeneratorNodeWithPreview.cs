1f8f2c905e3afcebcf48433215dabb1011345acc
Jordy <jordy_van_opstal@live.nl>
2020-12-11 14:18:20 +0100

    Add GeneratorNodeWithPreview
    text preview

---
+++ GeneratorNodeWithPreview.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Sirenix.OdinInspector;
+
+namespace Chinchillada.GeneratorGraph
+{
+    public abstract class GeneratorNodeWithPreview<T> : GeneratorNode<T>
+    {
+        [ShowInInspector, ReadOnly, UsedImplicitly]
+        private string preview;
+        
+        protected override void RenderPreview(T result)
+        {
+            this.preview = result.ToString();
+        }
+    }
+}
