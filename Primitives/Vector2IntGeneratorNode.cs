44f4c31d2271f7cea7259c91264018ae6614e858
Jordy <jordy_van_opstal@live.nl>
2020-12-21 19:32:37 +0100

    Add Vector2GeneratorNode

---
+++ Primitives/Vector2IntGeneratorNode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Generators
+{
+    public class Vector2IntGeneratorNode : GeneratorNodeWithPreview<Vector2Int>
+    {
+        [SerializeField] [Input] private int x;
+        [SerializeField] [Input] private int y;
+        
+        protected override Vector2Int GenerateInternal() => new Vector2Int(this.x, this.y);
+
+        protected override void UpdateInputs()
+        {
+            this.UpdateInput(ref this.x, nameof(this.x));
+            this.UpdateInput(ref this.y, nameof(this.y));
+        }
+    }
+}
