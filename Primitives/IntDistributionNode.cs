28f9a5b22c7cc703a45a6475f599976121158a87
Jordy <jordy_van_opstal@live.nl>
2021-01-10 01:16:49 +0100

    Add IntDistributionNode

---
+++ Primitives/IntDistributionNode.cs
@@ -0,0 +1,16 @@
+﻿namespace Generators
+{
+    using Chinchillada.Distributions;
+    using Distributions.Components;
+    using UnityEngine;
+
+    public class IntDistributionNode : GeneratorNodeWithPreview<int>
+    {
+        [SerializeField] private IDistribution<int> distribution = new IntRange();
+        
+        protected override int GenerateInternal()
+        {
+            return this.distribution.Sample();
+        }
+    }
+}
